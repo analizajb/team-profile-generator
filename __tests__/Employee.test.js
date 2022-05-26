5cec84ae37fd27c695d5784d6a723e1936ddcb82
analizajb <96861913+analizajb@users.noreply.github.com>
2022-05-26 13:36:46 -0700

    updating lines 6-8; changed 'this' to 'employee' - PASSED

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,9 +3,9 @@ const Employee = require('../lib/Employee');
 test('creates employee object', () => {
     const employee = new Employee('Analiza', 1738, 'analizaboehning@gmail.com');
 
-    expect(this.name).toEqual(expect.any(String));
-    expect(this.id).toEqual(expect.any(Number));
-    expect(this.email).toEqual(expect.any(String));
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
 
 test('gets employee name', () => {
