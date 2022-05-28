be865c26057b28eaa16c8f376245de7789334917
analizajb <96861913+analizajb@users.noreply.github.com>
2022-05-28 15:30:18 -0700

    adding engineer and intern prompts

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const promptUser = [
     {
         type: 'input',
         name: 'managerName',
-        message: "Please enter your team managers name (required)",
+        message: "Please enter your team managers name. (required)",
         validate: managerNameInput => {
             if (managerNameInput) {
                 return true;
@@ -78,27 +78,145 @@ const promptUser = [
             }
         }
     },
+    // IF ENGINEER IS SELECTED 
+    // Engineer Name
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: "Please enter your engineer's name. (required)",
+        validate: engineerName => {
+            if (engineerName) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Engineer ID
+    {
+        type: 'input',
+        name: 'engineerId',
+        message: "Please enter your engineer's ID. (required)",
+        validate: engineerId => {
+            if (engineerId) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Engineer email
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: "Please enter your engineer's email. (required)",
+        validate: engineerEmail => {
+            if (engineerEmail) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Engineer GitHub
+    {
+        type: 'input',
+        name: 'engineerGhub',
+        message: "Please enter your engineer's GitHub username. (required)",
+        validate: engineerGhub => {
+            if (engineerGhub) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    }, 
+    // TO DO: TAKE BACK TO MENU
+    // WHEN INTERN IS SELECTED
+    // Intern Name
+    {
+        type: 'input',
+        name: 'internName',
+        message: "Please enter your intern's name. (required)",
+        validate: internName => {
+            if (internName) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    }, 
+    // Intern ID
+    {
+        type: 'input',
+        name: 'internId',
+        message: "Please enter your intern's ID. (required)",
+        validate: internId => {
+            if (internId) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Intern email
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: "Please enter your intern's email. (required)",
+        validate: internEmail => {
+            if (internEmail) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Intern School
+    {
+        type: 'input',
+        name: 'internEdu',
+        message: "Please enter your intern's school name. (required)",
+        validate: internEdu => {
+            if (internEdu) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // TAKE USER BACK TO MENU
+    // CONSOLE LOG INPUT WAS GENERATED INTO INDEX FILE
+]
 
-// Function to generate prompts into the index.html
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err)
-            throw err;
-        console.log('Thank you for your input! Your team roster has been generated.')
-    });
-};
+// // Function to generate prompts into the index.html
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, (err) => {
+//         if (err)
+//             throw err;
+//         console.log('Thank you for your input! Your team roster has been generated.')
+//     });
+// },
 
-// Function to initialize HTML file that displays formatted team roster
-function init() {
-    inquirer.prompt(promptUser)
-    .then(function (userInput) {
-        console.log(userInput)
-        writeToFile("dist/index.html", generatePage(userInput));
-    });
-};
+// // Function to initialize HTML file that displays formatted team roster
+// function init() {
+//     inquirer.prompt(promptUser)
+//     .then(function (userInput) {
+//         console.log(userInput)
+//         writeToFile("dist/index.html", generatePage(userInput));
+//     });
+// },
 
-// Function call
-init();
+// // Function call
+// init();
 
 // Position title
     // {
