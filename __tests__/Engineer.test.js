14f51852711de6f0651c85591fab964405ad50ec
analizajb <96861913+analizajb@users.noreply.github.com>
2022-05-29 10:41:14 -0700

    passing test for Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,19 +2,19 @@ const Engineer = require('../lib/Engineer');
 
 // Create Engineer as an object
 test('create Engineer object', () => {
-    const engineer = new Engineer(any(String), any(Number), any(String), any(String));
+    const engineer = new Engineer('Analiza', 1738, 'analizaboehning@gmail.com', 'analizajb');
 
     expect(engineer.github).toEqual(expect.any(String));
 });
 
 test('gets value of Engineers GitHub', () => {
-    const engineer = new Engineer(any(String), any(Number), any(String), any(String));
+    const engineer = new Engineer('Analiza', 1738, 'analizaboehning@gmail.com', 'analizajb');
 
     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
 });
 
 test('gets role', () => {
-    const engineer = new Engineer(any(String), any(Number), any(String), any(String));
+    const engineer = new Engineer('Analiza', 1738, 'analizaboehning@gmail.com', 'analizajb');
 
     expect(engineer.getRole()).toEqual('Engineer');
 });
